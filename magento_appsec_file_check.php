fcbe180fb06821d1128418b6e04c97f9a68cd0cc
gaiterjones <paj@gaiterjones.com>
2015-10-23 14:18:04 +0200

    0.11

--- magento_appsec_file_check.php
+++ magento_appsec_file_check.php
@@ -12,8 +12,8 @@ if (!file_exists($_magentoPath. 'app/Mage.php')) {
 // SECURITY NOTICES AND QUERIES
 //
 $_securityNotices=array(
-	'APPSEC-1034, addressing bypassing custom admin URL' => array(
-		'text' => '',
+	'APPSEC-1034' => array(
+		'text' => 'addressing bypassing custom admin URL',
 		'exec' => array(
 			'path' => array(
 					$_magentoPath. 'app/code/*'
@@ -24,8 +24,8 @@ $_securityNotices=array(
 				)
 		),
 		'magentopath' => $_magentoPath),
-	'APPSEC-1063, addressing possible SQL injection' => array(
-		'text' => '',
+	'APPSEC-1063' => array(
+		'text' => 'addressing possible SQL injection',
 		'exec' => array(
 			'path' => array(
 					$_magentoPath. 'app/code/community/*',
@@ -38,8 +38,8 @@ $_securityNotices=array(
 			)
 		),
 		'magentopath' => $_magentoPath),
-	'APPSEC-1057, template processing method allows access to private information' => array(
-		'text' => '',
+	'APPSEC-1057' => array(
+		'text' => 'template processing method allows access to private information',
 		'exec' => array(
 			'path' => array(
 					$_magentoPath. 'app/code/community/*',
@@ -64,8 +64,8 @@ $_count=1;
 foreach ($_securityNotices as $_name => $_securityNotice)
 {
 
-	echo '['. $_count++. '] '. $_name. "\n";
-	echo doExec($_securityNotice)."\n\n";
+	echo '['. $_count++. '] '. $_name. ', '. $_securityNotice['text']. "\n";
+	echo doExec($_securityNotice,$_name)."\n\n";
 
 }
 
@@ -73,13 +73,14 @@ echo '***********************************'. "\n";
 exit;
 
 
-function doExec($_securityNotice)
+function doExec($_securityNotice,$_name)
 {
 	$_text='';	
 	$_exec=$_securityNotice['exec']['cmd'];	
 	
 	foreach ($_securityNotice['exec']['path'] as $_searchPath)
 	{
+		$_text=$_text.'looking in '. $_searchPath. "\n";			
 		
 		$_count=0;
 		$_search='';
@@ -87,8 +88,6 @@ function doExec($_securityNotice)
 		foreach ($_securityNotice['exec']['query'] as $_searchQuery)
 		{
 
-			$_text=$_text.'looking in '. $_searchPath. ' for '. $_searchQuery. "\n";
-		
 			exec($_exec. $_searchQuery. ' '. $_searchPath, $_output, $_status);
 			
 			if (1 === $_status)
@@ -100,19 +99,20 @@ function doExec($_securityNotice)
 
 			if (0 === $_status)
 			{
-				$_count=count($_output);
+				$_count=$_count + count($_output);
 				
 				foreach ($_output as $_line)
 				{
-					$_search=$_search.$_searchQuery. ' found in '. str_replace($_securityNotice['magentopath'],' ', $_line). "\n";
+					$_search=$_search.'['. $_name. '] '. $_searchQuery. ' found in '. "\033[31m". str_replace($_securityNotice['magentopath'],' ', $_line). "\033[0m\n";
 				}
 				
 			} else {
 				$_text=$_text. 'Command '. $_securityNotice['exec']['cmd']. ' failed with status: ' . $_status. "\n";
 			}
 			
-			$_text=$_text.$_count. ' effected files : '. "\n". $_search. "\n";
 		}
+		
+		$_text=$_text.$_count. ' affected files : '. "\n". $_search. "\n";
 	}
 	
 	return $_text;
